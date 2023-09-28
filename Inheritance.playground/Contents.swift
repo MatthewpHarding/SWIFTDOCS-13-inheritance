e9f7ecb567bb74b422b01523cb79404b497fa241
Matthew Harding <matthewpharding@gmail.com>
2023-09-29 00:23:47 +0100

    Converted playground page into single playground

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -165,3 +165,4 @@ print("AutomaticCar: \(automatic.description)")
 //: Any attempt to override a final method, property, or subscript in a subclass is reported as a compile-time error. Methods, properties, or subscripts that you add to a class in an extension can also be marked as final within the extension’s definition.
 //:
 //: You can mark an entire class as final by writing the final modifier before the class keyword in its class definition (final class). Any attempt to subclass a final class is reported as a compile-time error.
+
